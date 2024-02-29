2966f0af5c3e99c1cde085d3ebb5c6a4080e9c11
NandoApri26 <apriando.pratamaa@gmail.com>
2024-02-29 08:27:41 +0700

    Create Migration Camp Benefits

---
+++ database/migrations/2024_02_29_011216_create_camp_benefits_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCampBenefitsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('camp_benefits', function (Blueprint $table) {
+            $table->id();
+
+            // Step 1 relation on table
+            // $table->unsignedBigInteger('camp_id');
+            $table->foreignId('camp_id')->constrained(); // foreign key constraint to camps table, if camp is deleted, all its benefits will be deleted as well
+            $table->string('name');
+            $table->timestamps();
+
+            // Step 1 Relation on table
+            // $table->foreign('camp_id')->references('id')->on('camps')->onDelete('cascade'); // foreign key constraint to camps table, if camp is deleted, all its benefits will be deleted as well
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('camp_benefits');
+    }
+}
