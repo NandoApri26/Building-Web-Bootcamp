d41a15bf9aa8e2c6860ededecdd64df89b161d70
NandoApri26 <apriando.pratamaa@gmail.com>
2024-02-29 16:23:03 +0700

    Created CampBenefit seed class and update related migrations and models.

--- database/migrations/2024_02_29_011216_create_camp_benefits_table.php
+++ database/migrations/2024_02_29_011216_create_camp_benefits_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\Models\Camp;
 
 class CreateCampBenefitsTable extends Migration
 {
@@ -18,7 +19,7 @@ class CreateCampBenefitsTable extends Migration
 
             // Step 1 relation on table
             // $table->unsignedBigInteger('camp_id');
-            $table->foreignId('camp_id')->constrained();
+            $table->foreignIdFor(Camp::class)->constrained();
             $table->string('name');
             $table->timestamps();
 
--- database/migrations/2024_02_29_064057_create_checkouts_table.php
+++ database/migrations/2024_02_29_064057_create_checkouts_table.php
@@ -3,6 +3,9 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\Models\User;
+use App\Models\Camp;
+
 
 class CreateCheckoutsTable extends Migration
 {
@@ -15,8 +18,8 @@ class CreateCheckoutsTable extends Migration
     {
         Schema::create('checkouts', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained();
-            $table->foreignId('camp_id')->constrained();
+            $table->foreignIdFor(User::class)->constrained();
+            $table->foreignIdFor(Camp::class)->constrained();
             $table->string('card_number', 20);
             $table->date('expired');
             $table->string('cvv', 3);
