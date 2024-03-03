50b86a07232fd707487d53caf10ef62f7033994c
NandoApri26 <apriando.pratamaa@gmail.com>
2024-03-03 18:49:39 +0700

    Implement CheckoutController and update routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\User\CheckoutController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,13 +19,10 @@ Route::get('/', function () {
     return view('index');
 })->name('index');
 
-Route::get('/checkout/{camp:slug}', function () {
-    return view('checkout');
-})->name('checkout');
+Route::get('/checkout/success', [CheckoutController::class, 'success'])->name('checkout.success');
+Route::get('/checkout/{camp:slug}', [CheckoutController::class, 'create'])->name('checkout.create');
+Route::post('/checkout/{camp}', [CheckoutController::class, 'store'])->name('checkout.store');
 
-Route::get('/success-checkout', function() {
-    return view('success_checkout');
-})->name('success-checkout');
 
 // Sosialite Routes
 Route::get('/sign-in-google', [UserController::class, 'google'])
