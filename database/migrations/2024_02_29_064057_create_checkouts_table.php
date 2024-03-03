4c436ce799b7ac151cf10be51ec5191c7dadf4ee
NandoApri26 <apriando.pratamaa@gmail.com>
2024-03-03 18:48:43 +0700

    Refactor 'checkouts' table in migration file

--- database/migrations/2024_02_29_064057_create_checkouts_table.php
+++ database/migrations/2024_02_29_064057_create_checkouts_table.php
@@ -18,11 +18,11 @@ class CreateCheckoutsTable extends Migration
     {
         Schema::create('checkouts', function (Blueprint $table) {
             $table->id();
-            $table->foreignIdFor(User::class)->constrained();
-            $table->foreignIdFor(Camp::class)->constrained();
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('camp_id')->constrained();
             $table->string('card_number', 20);
             $table->date('expired');
-            $table->string('cvv', 3);
+            $table->string('cvc', 3);
             $table->boolean('is_paid')->default(false);
             $table->timestamps();
             $table->softDeletes(); // soft delete
