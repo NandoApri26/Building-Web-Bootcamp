5ad60058801ba60ee88fdaa174a7287869bcdf9d
NandoApri26 <apriando.pratamaa@gmail.com>
2024-03-03 18:49:06 +0700

    Refactor Checkout model's expired attribute handling

--- app/Models/Checkout.php
+++ app/Models/Checkout.php
@@ -15,7 +15,12 @@ class Checkout extends Model
         'camp_id',
         'card_number',
         'expired',
-        'cvv',
+        'cvc',
         'is_paid'
     ];
+
+    public function setExpiredAttribute($value)
+    {
+        $this->attributes['expired'] = date('Y-m-t', strtotime($value));
+    }
 }
