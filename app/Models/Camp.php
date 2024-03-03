6c1c1d7b394f6d118adff6105686f396cb0aca45
NandoApri26 <apriando.pratamaa@gmail.com>
2024-03-04 05:54:25 +0700

    Add registration checking to Camp model

--- app/Models/Camp.php
+++ app/Models/Camp.php
@@ -5,10 +5,27 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes; // Import the soft delete trait
+use Illuminate\Support\Facades\Auth;
+
+use App\Models\Checkout;
 
 class Camp extends Model
 {
     use HasFactory, softDeletes;
 
-    protected $fillable = ['title', 'price'];
+    protected $fillable = [
+        'title',
+        'price'
+    ];
+
+    public function getIsRegisteredAttribute()
+    {
+        if (!Auth::check()) {
+            return false;
+        }
+
+        return Checkout::where('user_id', Auth::id())
+            ->where('camp_id', $this->id)
+            ->exists();
+    }
 }
