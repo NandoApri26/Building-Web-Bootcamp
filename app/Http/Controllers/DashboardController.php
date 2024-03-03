aeff1a88cc431e9136614614c23dfcd493c907be
NandoApri26 <apriando.pratamaa@gmail.com>
2024-03-04 05:01:56 +0700

    Add checkout data to user dashboard view

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,11 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Checkout;
+
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('user.dashboard');
+        $checkouts = Checkout::with('Camp')->where('user_id', Auth::user()->id)->get();
+        return view('user.dashboard', [
+            'checkouts' => $checkouts
+        ]);
     }
 }
