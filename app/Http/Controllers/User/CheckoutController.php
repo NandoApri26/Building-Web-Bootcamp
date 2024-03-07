1dea3b595ef65997c68be3d1c4887de0fdae949d
NandoApri26 <apriando.pratamaa@gmail.com>
2024-03-07 08:28:14 +0700

    Remove the invoice function from the CheckoutController

--- app/Http/Controllers/User/CheckoutController.php
+++ app/Http/Controllers/User/CheckoutController.php
@@ -90,9 +90,4 @@ class CheckoutController extends Controller
     {
         return view('checkout.success');
     }
-
-    public function invoice(Checkout $checkout)
-    {
-        return $checkout;
-    }
 }
